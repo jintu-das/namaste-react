adcfa950d22e8b21eba715935350110f77649482
Jintu Das <jintudas047@gmail.com>
2023-07-27 08:08:01 +0530

    episode-1

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
-console.log(heading);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    {
+      id: "child",
+      key: "1",
+    },
+    [
+      React.createElement("h1", { key: "1.1" }, "I'm a h1 tag"),
+      React.createElement("h2", { key: "1.2" }, "I'm a h2 tag"),
+    ]
+  ),
+  React.createElement(
+    "div",
+    {
+      id: "child2",
+      key: "2",
+    },
+    [
+      React.createElement("h1", { key: "2.1" }, "I'm a h1 tag"),
+      React.createElement("h2", { key: "2.2" }, "I'm a h2 tag"),
+    ]
+  ),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
